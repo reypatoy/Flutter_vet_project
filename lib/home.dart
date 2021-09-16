7a96891fb79339af89547e29456fab7a485b831b
reypatoy <85288525+reypatoy@users.noreply.github.com>
2021-09-16 10:20:25 +0800

    Creating a sidebar drawer.

--- lib/home.dart
+++ lib/home.dart
@@ -1,35 +1,67 @@
 import 'package:flutter/material.dart';
 
+import './drawer.dart';
+
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Stack(
-      children: <Widget>[
-        FractionallySizedBox(
-          widthFactor: 1,
-          heightFactor: 1,
-          child: Container(
-            color: Colors.green,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'BEASTFRIEND VETERINARY CLINIC',
+          style: const TextStyle(
+            fontSize: 18,
+          ),
+        ),
+      ),
+      drawer: new SideDrawer(),
+      body: Stack(
+        children: <Widget>[
+          FractionallySizedBox(
+            widthFactor: 1,
+            heightFactor: 1,
             child: Container(
-              child: Image.asset(
-                'assets/home_logo.jpg',
-                fit: BoxFit.fitHeight,
+              color: Colors.green,
+              child: Container(
+                child: Image.asset(
+                  'assets/home_logo.jpg',
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
           ),
-        ),
-        Container(
-          width: 90,
-          height: 90,
-          color: Colors.yellow,
-        ),
-        Container(
-          width: 80,
-          height: 80,
-          color: Colors.blue,
-        ),
-      ],
+          Container(
+            alignment: Alignment.center,
+            child: Container(
+              width: 350,
+              height: 100,
+              child: Column(
+                children: [
+                  Text(
+                    'BeastFriend Veterinary Clinic',
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.green,
+                      fontSize: 26,
+                    ),
+                  ),
+                  Text(
+                    'Mastering The Art Of Pet Care',
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.green,
+                      fontSize: 18,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,24 @@
 import 'package:flutter/material.dart';
 
-import './question.dart';
 import './home.dart';
+import './drawer.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return MyAppState();
-  }
+void main() {
+  runApp(MaterialApp(
+    title: 'Navigation',
+    home: MyApp(),
+  ));
 }
 
-class MyAppState extends State<MyApp> {
-  var index = 0;
-  void answered() {
-    setState(() {
-      index = index + 1;
-      print(index);
-    });
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var questions = ['My first question?', 'My second question?'];
-
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text('BeastFriend Veterinary Clinic'),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      body: Home(),
-      drawer: Container(
-        width: 200,
-        child: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: <Widget>[
-              UserAccountsDrawerHeader(
-                accountName: Text('Test123'),
-                accountEmail: Text('test@123.com'),
-                currentAccountPicture: Image.network(
-                    'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a89c3e38-b6f3-48a0-9f9e-df9a0129fb93/daghh5x-4a77b3ec-fd4f-4d17-9f84-5963a8cb5c03.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2E4OWMzZTM4LWI2ZjMtNDhhMC05ZjllLWRmOWEwMTI5ZmI5M1wvZGFnaGg1eC00YTc3YjNlYy1mZDRmLTRkMTctOWY4NC01OTYzYThjYjVjMDMucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.dWTFMrwnbAbj5TtUp9U_vQsohW7MnkRPymzR5wZQoV8'),
-              ),
-              ListTile(
-                title: const Text('HOME'),
-                onTap: () {
-                  // Update the state of the app.
-                  // ...
-                  print('yes');
-                },
-              ),
-              ListTile(
-                title: const Text('SERVICES'),
-                onTap: () {
-                  // Update the state of the app.
-                  // ...
-                },
-              ),
-              ListTile(
-                title: const Text('APPOINTMENTS'),
-                onTap: () {
-                  // Update the state of the app.
-                  // ...
-                },
-              ),
-              ListTile(
-                title: const Text('ACCOUNT'),
-                onTap: () {
-                  // Update the state of the app.
-                  // ...
-                },
-              ),
-            ],
-          ),
-        ),
-      ),
-    ));
+      home: Home(),
+    );
   }
 }
